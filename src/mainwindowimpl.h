a00e5fcd7d9b50ca74e73bb624e3937833740ffb
PanteR <panter.dsd@gmail.com>
2011-11-19 12:43:32 +0400

    Styling.

--- src/mainwindowimpl.h
+++ src/mainwindowimpl.h
@@ -24,53 +24,53 @@ class MainWindowImpl : public QMainWindow
 {
 	Q_OBJECT
 private:
-	QTableWidget					*table_;
-	QStatusBar						*statusBar_;
-	QToolBar							*fileToolbar_;
-	QToolBar							*reestrToolbar_;
-	QMenuBar							*mainMenu_;
-	QLabel								*sumLabel_;
+	QTableWidget *table_;
+	QStatusBar *statusBar_;
+	QToolBar *fileToolbar_;
+	QToolBar *reestrToolbar_;
+	QMenuBar *mainMenu_;
+	QLabel *sumLabel_;
 
-	QGroupBox						*headerGroup_;
-	QLabel								*organizationNameLabel_;
-	QLineEdit							*organizationNameEdit_;
-	QLabel								*reestrNumberLabel_;
-	QSpinBox							*reestrNumberEdit_;
-	QLabel								*filialNameLabel_;
-	QLineEdit							*filialNameEdit_;
-	QLabel								*schetOrganizationLabel_;
-	QLineEdit							*schetOrganizationEdit_;
-	QLabel								*dogovorNumberLabel_;
-	QLineEdit							*dogovorNumberEdit_;
-	QLabel								*dogovorDateLabel_;
-	QDateEdit							*dogovorDateEdit_;
-	QLabel								*platNumberLabel_;
-	QLineEdit							*platNumberEdit_;
-	QLabel								*platDateLabel_;
-	QDateEdit							*platDateEdit_;
-	QLabel								*vidLabel_;
-	QComboBox						*vidEdit_;
+	QGroupBox *headerGroup_;
+	QLabel *organizationNameLabel_;
+	QLineEdit *organizationNameEdit_;
+	QLabel *reestrNumberLabel_;
+	QSpinBox *reestrNumberEdit_;
+	QLabel *filialNameLabel_;
+	QLineEdit *filialNameEdit_;
+	QLabel *schetOrganizationLabel_;
+	QLineEdit *schetOrganizationEdit_;
+	QLabel *dogovorNumberLabel_;
+	QLineEdit *dogovorNumberEdit_;
+	QLabel *dogovorDateLabel_;
+	QDateEdit *dogovorDateEdit_;
+	QLabel *platNumberLabel_;
+	QLineEdit *platNumberEdit_;
+	QLabel *platDateLabel_;
+	QDateEdit *platDateEdit_;
+	QLabel *vidLabel_;
+	QComboBox *vidEdit_;
 
-	QAction								*actionNew_;
-	QAction								*actionImport_;
-	QAction								*actionOpen_;
-	QAction								*actionSave_;
-	QAction								*actionSaveAs_;
-	QAction								*actionAdd_;
-	QAction								*actionEdit_;
-	QAction								*actionDelete_;
-	QAction								*actionChangeMoney_;
-	QAction								*actionNullMoney_;
-	QAction								*actionSaveHeader_;
-	QAction								*actionAbout_;
-	QAction								*actionAboutQt_;
-	QAction								*actionExit_;
-	QAction								*actionOut_;
-	QAction								*actionPreferences_;
+	QAction *actionNew_;
+	QAction *actionImport_;
+	QAction *actionOpen_;
+	QAction *actionSave_;
+	QAction *actionSaveAs_;
+	QAction *actionAdd_;
+	QAction *actionEdit_;
+	QAction *actionDelete_;
+	QAction *actionChangeMoney_;
+	QAction *actionNullMoney_;
+	QAction *actionSaveHeader_;
+	QAction *actionAbout_;
+	QAction *actionAboutQt_;
+	QAction *actionExit_;
+	QAction *actionOut_;
+	QAction *actionPreferences_;
 
-	QSettings							*applicationSettings_;
-	QString								currentFile_;
-	QString								version_;
+	QSettings *applicationSettings_;
+	QString currentFile_;
+	QString version_;
 
 protected:
 	void closeEvent (QCloseEvent *event);
